4bc818aed29c3ad3cb8eb9c4f0550c6a95b43aae
Zhu, Yuanzheng <jeffzyz@gatech.edu>
2017-04-14 23:36:06 -0400

    listview

--- app/src/main/java/gatech/nav/MapsActivity.java
+++ app/src/main/java/gatech/nav/MapsActivity.java
@@ -25,7 +25,6 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.Dash;
-import com.google.android.gms.maps.model.Dot;
 import com.google.android.gms.maps.model.Gap;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
